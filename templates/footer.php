54591044dc16108ef6e804897ab9eafea2e5adcf
Nicolas Badin <50690360+N1coc4colA@users.noreply.github.com>
2025-02-14 17:31:44 +0100

    Add template files

---
+++ templates/footer.php
@@ -0,0 +1,37 @@
+<footer class="bg-dark text-white text-center py-3">
+    <div class="container">
+      <div class="row align-items-center mb-2">
+        <div class="col-auto">
+          <a href="tel:+33243491859"><i class="fa-solid fa-square-phone"></i></a>
+        </div>
+        <div class="col text-start">
+          <p class="mb-0">Téléphone: <a href="tel:+33243491859">+33243491859</a></p>
+        </div>
+      </div>
+      <div class="row align-items-center mb-2">
+        <div class="col-auto">
+          <a href="mailto:orfevrelaval@gmail.com"><i class="fa-solid fa-envelope"></i></a>
+        </div>
+        <div class="col text-start">
+          <p class="mb-0">Mail: <a href="mailto:orfevrelaval@gmail.com">orfevrelaval@gmail.com</a></p>
+        </div>
+      </div>
+      <div class="row align-items-center mb-2">
+        <div class="col-auto">
+          <a href="https://www.facebook.com/OrfevreLaval/"><i class="fa-brands fa-square-facebook"></i></a>
+        </div>
+        <div class="col text-start">
+          <p class="mb-0">Visiter <a href="https://www.facebook.com/OrfevreLaval/">la page Facebook</a></p>
+        </div>
+      </div>
+      <div class="row align-items-center">
+        <div class="col-auto">
+          <a href="geo:48.0681001,-0.7746813,597?q=L'Orfèvre"><i class="fa-solid fa-location-dot"></i></a>
+        </div>
+        <div class="col text-start">
+          <p class="mb-0">Voir <a href="https://www.google.com/maps/dir/?api=1&destination=48.0683%2C-0.77209">sur Google Maps</a></p>
+        </div>
+      </div>
+    </div>
+    <p>GPLv3+, Images pouvant être sujettes à droit d'auteur.</p>
+</footer>
---
+++ templates/header.php
@@ -0,0 +1,25 @@
+<header>
+    <nav class="navbar navbar-expand-lg fixed-top border-bottom bg-body" data-bs-theme="dark">
+        <div class="container-fluid">
+            <a class="navbar-brand outfit-font" href="#">
+              <img src="assets/favicon-light.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a page-name="index" class="nav-link" href="index.html">Accueil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a page-name="menu" class="nav-link" href="menu.html">Menu</a>
+                    </li>
+                    <li class="nav-item">
+                        <a page-name="reservation" class="nav-link" href="reservation.html">Réservation</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
