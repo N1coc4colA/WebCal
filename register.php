56caff615ff26f80a47e47449aebcedd97cdf771
Nicolas Badin <50690360+N1coc4colA@users.noreply.github.com>
2025-02-16 02:12:43 +0100

    Fix registration page
    Fix wrong redirection
    Fix regexp to require uppercase, lowercase, special char, and digit, with min len of 8, and max of 30.

--- register.php
+++ register.php
@@ -24,7 +24,7 @@
             <div class="card mt-5">
               <div class="card-body">
                 <h5 class="card-title">Créer un compte</h5>
-                <form action="process_registration.php" method="POST" onsubmit="return validateForm()">
+                <form action="registration-process.php" method="POST" onsubmit="return validateForm()">
                     <div class="input-group mb-3">
                         <label class="input-group-text" for="lastname">Nom</label>
                         <input class="form-control" type="text" id="lastname" name="lastname" maxlength="30" minlength="3" pattern="^[A-Za-zÀ-ÿ'\\-\\s]+$" required>
@@ -51,12 +51,12 @@
                     </div>
                     <div class="input-group mb-3">
                         <label class="input-group-text" for="password">Mot de passe</label>
-                        <input class="form-control" type="password" id="password" name="password" pattern="(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{8,}" maxlength="30" required>
+                        <input class="form-control" type="password" id="password" name="password" pattern="(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).{8,}" minlength="8" maxlength="30" required>
                         <i class="input-group-text bi bi-eye-slash" id="togglePassword"></i>
                     </div>
                     <div class="input-group mb-3">
                         <label class="input-group-text" for="password">Confirmez</label>
-                        <input class="form-control" type="password" id="passwordConf" name="password" pattern="(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{8,}" maxlength="30" required>
+                        <input class="form-control" type="password" id="passwordConf" name="password" pattern="(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).{8,}" minlength="8" maxlength="30" required>
                         <i class="input-group-text bi bi-eye-slash" id="togglePasswordConf"></i>
                     </div>
                     <div class="d-flex flex-column flex-sm-row justify-content-center mt-5">
