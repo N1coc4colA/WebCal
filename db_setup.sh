fc27a1097b7e6f950e18f878c666913baf140f78
Nicolas Badin <50690360+N1coc4colA@users.noreply.github.com>
2025-02-16 02:52:24 +0100

    Fix DB setup
    Fix script file (creation order, auth & parameters)
    Fix SQL file (conditions missing commas or added ones, unused references to tables)

--- db_setup.sh
+++ db_setup.sh
@@ -6,13 +6,27 @@ DB_USER="webcal-user"
 DB_PASSWORD="webcal-pw"
 DB_NAME="webcal"
 
+
 # Path to the SQL script
 SQL_FILE="db_setup.sql"
 
+# Authorize user to access the DB
+ACCESS_GRANT="
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'$DB_HOST' IDENTIFIED BY '$DB_PASSWORD';
+FLUSH PRIVILEGES;"
+
+# Create DB first & authorize
+echo "Creating DB..."
+sudo mysql --host=localhost --user=root --password=$DB_PASSWORD <<< "CREATE DATABASE IF NOT EXISTS webcal;"
+
 # Execute the SQL script using mysql
-mysql -h $DB_HOST -u $DB_USER -p$DB_PASSWORD $DB_NAME < $SQL_FILE
+echo "Building DB..."
+sudo mysql --host=$DB_HOST --user=root --password=$DB_PASSWORD $DB_NAME < $SQL_FILE
+
+echo "Granting privileges..."
+sudo mysql --host=localhost --user=root --password=$DB_PASSWORD <<< "$ACCESS_GRANT"
 
 # For PostgreSQL, you can use the following command:
 # psql -h $DB_HOST -U $DB_USER -d $DB_NAME -f $SQL_FILE
 
-echo "Database setup complete."
+echo "DB setup complete."
--- db_setup.sql
+++ db_setup.sql
@@ -1,9 +1,7 @@
--- Create the database
-CREATE DATABASE webcal;
 USE webcal;
 
 -- Create Table USR_DT
-CREATE TABLE USR_DT (
+CREATE TABLE IF NOT EXISTS USR_DT (
     id INT PRIMARY KEY,                         -- UID
     name VARCHAR(30),                           -- Name (family) of the person
     surname VARCHAR(30),                        -- Surname (usual name) of the person
@@ -13,12 +11,18 @@ CREATE TABLE USR_DT (
     address VARCHAR(50),                        -- Address of the person
     salt CHAR(8),                               -- Salt for the password
     pwh CHAR(255),                              -- Hashed password
-    sub BOOLEAN DEFAULT FALSE,                  -- Submitted, means that the user validated his mail.
-    FOREIGN KEY (udid) REFERENCES USR_DT(id)    --
+    sub BOOLEAN DEFAULT FALSE                   -- Submitted, means that the user validated his mail.
+);
+
+-- Create Table INFO_DT, holding info about a rendez-vous
+CREATE TABLE IF NOT EXISTS INFO_DT (
+    id INT PRIMARY KEY,                         -- UID
+    ar_id INT,                                  -- UID of the source range
+    msg VARCHAR(200)                            -- Message, note, of the people who made the reservation
 );
 
 -- Create Table AR_DT, Available Ranges for a rendez-vous
-CREATE TABLE AR_DT (
+CREATE TABLE IF NOT EXISTS AR_DT (
     id INT PRIMARY KEY,                             -- UID
     src INT,                                        -- UID of the people who owns the range
     dst INT,                                        -- UID of the people who made a reservation
@@ -33,20 +37,12 @@ CREATE TABLE AR_DT (
     FOREIGN KEY (info_id) REFERENCES INFO_DT(id)    --
 );
 
--- Create Table INFO_DT, holding info about a rendez-vous
-CREATE TABLE INFO_DT (
-    id INT PRIMARY KEY,                         -- UID
-    ar_id INT,                                  -- UID of the source range
-    msg VARCHAR(200),                           -- Message, note, of the people who made the reservation
-    FOREIGN KEY (ar_id) REFERENCES AR_DT(id)    --
-);
-
 -- Create Table PENDING_DT, holding pending registrations
-CREATE TABLE PENDING_DT (
+CREATE TABLE IF NOT EXISTS PENDING_DT (
     id INT PRIMARY KEY,                         -- UID
     sub_date DATE,                              -- submission date of the mail validation request
     sub_time TIME,                              -- Start time of the mail validation request
-    validator VARCHAR(20)                       -- Random string used to validate the account (and avoid others validating others' accounts)
+    validator VARCHAR(20),                      -- Random string used to validate the account (and avoid others validating others' accounts)
     src INT,                                    -- UID of the person who requested mail validation
     FOREIGN KEY (src) REFERENCES USR_DT(id)
 );
