61f971ef761de4b2487607c072fe44d6f518230e
Nicolas Badin <50690360+N1coc4colA@users.noreply.github.com>
2025-02-20 23:40:32 +0100

    Proper redirections for PW change

--- change-password.php
+++ change-password.php
@@ -7,7 +7,7 @@
   }
 
   if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    header("Location: settings.php?pw-error-meth")
+    header("Location: settings.php?pw-error-meth");
     exit;
   }
 
@@ -23,19 +23,19 @@
     $nPassword = (string) mb_strimwidth(htmlspecialchars($_POST['nPassword']), 0, 30, "");
     // We don't care about confirmation password. Should be validated user-side only.
 
-    $stmt = $pdo->prepare("SELECT pwh FROM USR_DT WHERE id = ?");
+    $stmt = $pdo->prepare("SELECT pwh FROM USR_DT WHERE id=?");
     $stmt->execute([$_SESSION["id"]]);
 
     $row = $stmt->fetch(PDO::FETCH_ASSOC);
     $pwh = $row["pwh"];
 
     if (!password_verify($password, $pwh)) {
-      header("Location: settings.php?pw-error");
+      header("Location: settings.php#pw?pw-error");
       exit;
     }
 
     $hashedPassword = password_hash($nPassword, PASSWORD_BCRYPT);
-  
+
     // Update information
     $stmt = $pdo->prepare("UPDATE USR_DT SET pwh=? WHERE (id=?)");
     $stmt->execute([$hashedPassword, $_SESSION["id"]]);
@@ -43,4 +43,6 @@
     header("Location: error.php?error=sql-error.html");
     exit;
   }
+
+  header("Location: settings.php#pw");
 ?>
