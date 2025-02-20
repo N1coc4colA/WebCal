80528dbaa0bb9136a7bbc32a17edd2f6b3ecdc07
Nicolas Badin <50690360+N1coc4colA@users.noreply.github.com>
2025-02-20 23:16:26 +0100

    Enable info update in settings

--- update-information.php
+++ update-information.php
@@ -18,45 +18,50 @@
   $phone = mb_strimwidth(htmlspecialchars(trim($_POST['phone'])), 0, 10, "");
   $email = (string) mb_strimwidth(filter_var(trim($_POST['email']), FILTER_VALIDATE_EMAIL), 0, 30, "");
 
-  if (!(!preg_match('/^[0-9]{10}$/', $phone))) {
+  if (!preg_match('/^[0-9]{10}$/', $phone)) {
     header("Location: settings.php?phone-error");
     exit;
   }
 
+  $dsn = 'mysql:host=localhost;dbname=webcal;charset=utf8';
+  $username = 'webcal-user';
+  $password_db = 'webcal-pw';
+
   try {
-    $dsn = 'mysql:host=localhost;dbname=webcal;charset=utf8';
-    $username = 'webcal-user';
-    $password_db = 'webcal-pw';
-  
     $pdo = new PDO($dsn, $username, $password_db);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  
-    $stmt = $pdo->prepare("SELECT COUNT(*) FROM USR_DT WHERE email = ?");
+
+    $stmt = $pdo->prepare("SELECT id FROM USR_DT WHERE email = ?");
     $stmt->execute([$email]);
-    $emailExists = $stmt->fetchColumn();
-  
-    if (!$emailExists) {
+    $ids = $stmt->fetchAll(PDO::FETCH_COLUMN);
+    $rowCount = count($ids);
+
+    if ($rowCount > 1) { // Looks like we got an error from somewhere. Very unlikely.
       header("Location: settings.php?mail-error");
       exit;
     }
-  
+    if ($rowCount == 1 && $_SESSION["id"] != $ids[0]) { // Not the current account.
+      header("Location: settings.php?mail-error");
+      exit;
+    }
+
     // Update information
     // Do not forget to invalidate user verification.
-    $stmt = $pdo->prepare("UPDATE USR_DT SET name=?, surname=?, birthdate=?, address=?, phone=?, email=?, sub=?) WHERE (id=?)");
-    $stmt->execute([$name, $surname, $birthdate, $address, $phone, $email, false, $_SESSION["id"]]);
-  
+    $stmt = $pdo->prepare("UPDATE USR_DT SET name=?, surname=?, birthdate=?, address=?, phone=?, email=?, sub=? WHERE (id=?)");
+    $stmt->execute([$name, $surname, $birthdate, $address, $phone, $email, 0, $_SESSION["id"]]);
+
     // [TODO] Send verification mail
     // Generate entry first
     $date = date('Y-m-d');
     $time = date('H:i:s');
-  
+
     $verificationCode = mb_strimwidth(bin2hex(random_bytes(20)), 0, 30, "");
     $verificationLink = "http://localhost/registration-success.php?code=$verificationCode";
-  
+
     // Store verification code
     $stmt = $pdo->prepare("INSERT INTO PENDING_DT (sub_date, sub_time, validator, src) VALUES (?, ?, ?, ?)");
     $stmt->execute([$date, $time, $verificationCode, $_SESSION["id"]]);
-  
+
     $subject = "Vérification de votre email";
     $message = "Bonjour $surname,\n\nCliquez sur le lien suivant pour vérifier votre email :\n$verificationLink\n\nCordialement,\nL'équipe d'inscription.";
     $headers = "From: noreply@localhost" . "\r\n";
@@ -64,4 +69,6 @@
     header("Location: error.php?error=sql-error.html");
     exit;
   }
+
+  header("Location: settings.php");
 ?>
