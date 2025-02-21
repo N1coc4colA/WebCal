55f925d05289bcc8aa9cf371922a7cc875bdfc40
Nicolas Badin <50690360+N1coc4colA@users.noreply.github.com>
2025-02-21 16:24:32 +0100

    Add SAN & VAL funcs for inputs

--- utils.php
+++ utils.php
@@ -13,4 +13,32 @@
 
     return $pdo;
   }
+
+  function san_pw($pw) {
+    return (string) mb_strimwidth(htmlspecialchars($pw), 0, 30, "");
+  }
+
+  function san_phone($phone) {
+    return (int) san_string($phone, 10);
+  }
+
+  function san_mail($mail) {
+    return (string) san_string($mail, 30);
+  }
+
+  function san_string($str, $len) {
+    return (string) mb_strimwidth(htmlspecialchars(trim($str)), 0, $len, "");
+  }
+
+  function validate_mail($mail) {
+    return filter_var($mail, FILTER_VALIDATE_EMAIL);
+  }
+
+  function validate_phone($phone) {
+    return preg_match('/^[0-9]{10}$/', $phone);
+  }
+
+  function validate_pw($pw) {
+    return preg_match('/(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{8,}/', $pw);
+  }
 ?>
