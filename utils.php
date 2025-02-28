e8a9380e937eee72975ad2f5e44307e091620c3c
Nicolas Badin <50690360+N1coc4colA@users.noreply.github.com>
2025-02-28 12:46:31 +0100

    Add send mail feature

--- utils.php
+++ utils.php
@@ -75,4 +75,55 @@
     }
     set_error_handler("exception_error_handler");
   }
+
+  use PHPMailer\PHPMailer\PHPMailer;
+  use PHPMailer\PHPMailer\SMTP;
+  use PHPMailer\PHPMailer\Exception;
+
+  function sendMail($target, $subject, $message, $plain) {
+    //Import PHPMailer classes into the global namespace
+    //These must be at the top of your script, not inside a function
+
+    require 'PHPMailer/src/Exception.php';
+    require 'PHPMailer/src/PHPMailer.php';
+    require 'PHPMailer/src/SMTP.php';
+
+    //Create an instance; passing `true` enables exceptions
+    $mail = new PHPMailer(true);
+
+    try {
+      //Server settings
+      $mail->isSMTP();                                    //Send using SMTP
+      $mail->Host       = 'smtp.gmail.com';               //Set the SMTP server to send through
+      $mail->SMTPAuth   = true;                           //Enable SMTP authentication
+      $mail->Username   = '';      //SMTP username
+      $mail->Password   = '';          //SMTP password
+      $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; //Enable implicit TLS encryption
+      $mail->Port       = 587;                            //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+      $mail->SMTPOptions = array(
+          "ssl" => array(
+              "verify_peer" => false,
+              "verify_peer_name" => false,
+              "allow_self_signed" => true
+          )
+      );
+
+      //Recipients
+      $mail->setFrom("webreservcall@gmail.com", "WebCal");
+      $mail->addAddress($target);
+
+      //Content
+      $mail->isHTML(true);                                //Set email format to HTML
+      $mail->Subject = $subject;
+      $mail->Body    = $message;
+      $mail->AltBody = $plain;
+
+      $mail->send();
+    } catch (Exception $e) {
+      return false;
+    }
+
+    return true;
+  }
 ?>
