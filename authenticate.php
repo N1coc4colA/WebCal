32d0cfcfcbb0b2c0f821dba6286060032462cbe8
Nicolas Badin <50690360+N1coc4colA@users.noreply.github.com>
2025-02-28 13:16:31 +0100

    Check user mail validation before login

--- authenticate.php
+++ authenticate.php
@@ -22,25 +22,30 @@
             try {
                 $pdo = connectDB();
 
-                // [TODO] Check that the user validated mail
                 $stmt = $pdo->prepare("SELECT COUNT(*) FROM USR_DT WHERE email = ?");
                 $stmt->execute([$email]);
                 $emailExists = $stmt->fetchColumn();
 
                 if ($emailExists) {
-                    $stmt = $pdo->prepare("SELECT id, pwh FROM USR_DT WHERE email = ?");
+                    $stmt = $pdo->prepare("SELECT id, pwh, sub FROM USR_DT WHERE email = ?");
                     $stmt->execute([$email]);
 
                     $row = $stmt->fetch(PDO::FETCH_ASSOC);
                     $id = $row["id"];
                     $pwh = $row["pwh"];
+                    $sub = $row["sub"];
 
                     $hashed = password_hash($password, PASSWORD_BCRYPT);
 
                     if (password_verify($password, $pwh)) {
-                        $_SESSION["id"] = $id;
-
-                        header("Location: calendar.php");
+                        if ($sub == true) {
+                            $_SESSION["id"] = $id;
+    
+                            header("Location: calendar.php");
+                        } else {
+                            header("Location: connect.php?mail-validate");
+                            exit;
+                        }
                     } else {
                         header("Location: connect.php?error");
                         exit;
